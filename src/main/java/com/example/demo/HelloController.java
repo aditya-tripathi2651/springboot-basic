eae5934d0e2b17791a5ba902154255328af95148
ECI-Aditya <atripathi@eci.com>
2025-07-22 18:57:46 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String greet() {
-        return "Hello from Spring Boot! Current time: " + java.time.LocalDateTime.now();
+        return "Hello from Spring Boot!! Current time: " + java.time.LocalDateTime.now();
     }
 }
